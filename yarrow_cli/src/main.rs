0ac150d97c6f2b3ac0cc00964ed2b8f503b7f7a7
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-24 23:50:50 +0200

    Update

--- yarrow_cli/src/main.rs
+++ yarrow_cli/src/main.rs
@@ -2,15 +2,7 @@ fn main() {
     let matches = clap::Command::new("")
         .version("0.0.1")
         .arg_required_else_help(true)
-        .subcommand(
-            clap::Command::new("run").about("").arg(
-                clap::Arg::new("quiet")
-                    .short('q')
-                    .long("quiet")
-                    .action(clap::ArgAction::SetTrue)
-                    .required(false),
-            ),
-        )
+        .subcommand(clap::Command::new("run").about(""))
         .subcommand(
             clap::Command::new("build")
                 .about("")
@@ -20,18 +12,29 @@ fn main() {
                         .long("optimization")
                         .value_name("number")
                         .action(clap::ArgAction::Set)
+                        .help("")
+                        .global(true)
                         .required(false),
                 )
-                .subcommand(
-                    clap::Command::new("run").about("").arg(
-                        clap::Arg::new("optimization")
-                            .short('O')
-                            .long("optimization")
-                            .value_name("number")
-                            .action(clap::ArgAction::Set)
-                            .required(false),
-                    ),
-                ),
+                .arg(
+                    clap::Arg::new("quiet")
+                        .short('q')
+                        .long("quiet")
+                        .action(clap::ArgAction::SetTrue)
+                        .help("")
+                        .global(true)
+                        .required(false),
+                )
+                .arg(
+                    clap::Arg::new("verbose")
+                        .short('v')
+                        .long("verbose")
+                        .action(clap::ArgAction::SetTrue)
+                        .help("")
+                        .global(true)
+                        .required(false),
+                )
+                .subcommand(clap::Command::new("run").about("")),
         )
         .subcommand(clap::Command::new("aliases").about(""))
         .subcommand(clap::Command::new("init").about(""))
@@ -39,19 +42,40 @@ fn main() {
         .subcommand(clap::Command::new("add").about(""))
         .subcommand(clap::Command::new("update").about(""))
         .subcommand(clap::Command::new("remove").about(""))
-        .subcommand(clap::Command::new("lsp").about(""))
         .subcommand(clap::Command::new("fmt").about(""))
+        .subcommand(clap::Command::new("lsp").about(""))
         .get_matches();
 
-    if let Some(matches) = matches.subcommand_matches("run") {
-        if matches.get_flag("quiet") {
-            println!("it's quiest here");
-        }
-    }
+    match matches.subcommand() {
+        Some(("run", matches)) => {
+            if matches.get_flag("quiet") {
+                todo!("Implement quiet mode in run");
+            }
 
-    if let Some(matches) = matches.subcommand_matches("build") {
-        if let Some(optimization) = matches.get_one::<String>("optimization") {
-            println!("{}", optimization);
+            if matches.get_flag("verbose") {
+                todo!("Implement verbose mode in run")
+            }
+
+            match matches.subcommand() {
+                _ => {
+                    todo!("Implement run command");
+                }
+            }
         }
+        Some(("build", matches)) => {
+            if let Some(optimization) = matches.get_one::<String>("optimization") {
+                todo!("Implement optimization level in build")
+            }
+
+            match matches.subcommand() {
+                Some(("run", matches)) => {
+                    todo!("Implement run command in build")
+                }
+                _ => {
+                    todo!("Implement build command")
+                }
+            }
+        }
+        _ => unreachable!(),
     }
 }
