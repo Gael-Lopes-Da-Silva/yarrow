6014d1705a4441ec1de3928f7fd74512dda7f54b
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-28 03:12:34 +0200

    Update tokenizer

--- src/core/lexer/token.go
+++ src/core/lexer/token.go
@@ -93,6 +93,44 @@ const (
 	KW_ALIAS
 	KW_SELF
 	KW_END
+
+	TYPE_U8
+	TYPE_U16
+	TYPE_U32
+	TYPE_U64
+	TYPE_U128
+	TYPE_I8
+	TYPE_I16
+	TYPE_I32
+	TYPE_I64
+	TYPE_I128
+	TYPE_F16
+	TYPE_F32
+	TYPE_F64
+	TYPE_F128
+	TYPE_BOOL
+	TYPE_VOID
+	TYPE_STRING
+	TYPE_ARRAY
+	TYPE_VECTOR
+	TYPE_HASHMAP
+	TYPE_STACK
+	TYPE_QUEUE
+
+	TYPE_POINTER
+	TYPE_USIZE
+	TYPE_ISIZE
+	TYPE_C_CHAR
+	TYPE_C_SHORT
+	TYPE_C_USHORT
+	TYPE_C_INT
+	TYPE_C_UINT
+	TYPE_C_LONG
+	TYPE_C_ULONG
+	TYPE_C_LONGLONG
+	TYPE_C_ULONGLONG
+	TYPE_C_DOUBLE
+	TYPE_C_LONGDOUBLE
 )
 
 type Token struct {
@@ -137,6 +175,44 @@ var reservedKeywords = map[string]TokenKind{
 	"alias":     KW_ALIAS,
 	"self":      KW_SELF,
 	"end":       KW_END,
+
+	"u8":   TYPE_U8,
+	"u16":  TYPE_U16,
+	"u32":  TYPE_U32,
+	"u64":  TYPE_U64,
+	"u128": TYPE_U128,
+	"i8":   TYPE_I8,
+	"i16":  TYPE_I16,
+	"i32":  TYPE_I32,
+	"i64":  TYPE_I64,
+	"i128": TYPE_I128,
+	"f16":  TYPE_F16,
+	"f32":  TYPE_F32,
+	"f64":  TYPE_F64,
+	"f128": TYPE_F128,
+	"bool": TYPE_BOOL,
+	"void": TYPE_VOID,
+	"string": TYPE_STRING,
+	"array": TYPE_ARRAY,
+	"vector": TYPE_VECTOR,
+	"hashmap": TYPE_HASHMAP,
+	"stack": TYPE_STACK,
+	"queue": TYPE_QUEUE,
+
+	"pointer": TYPE_POINTER,
+	"usize": TYPE_USIZE,
+	"isize": TYPE_ISIZE,
+	"c_char": TYPE_C_CHAR,
+	"c_short": TYPE_C_SHORT,
+	"c_ushort": TYPE_C_USHORT,
+	"c_int": TYPE_C_INT,
+	"c_uint": TYPE_C_UINT,
+	"c_long": TYPE_C_LONG,
+	"c_ulong": TYPE_C_ULONG,
+	"c_longlong": TYPE_C_LONGLONG,
+	"c_ulonglong": TYPE_C_ULONGLONG,
+	"c_double": TYPE_C_DOUBLE,
+	"c_longdouble": TYPE_C_LONGDOUBLE,
 }
 
 func (token Token) Debug() {
